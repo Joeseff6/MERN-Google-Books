b861ee1a969b94172ab7fd15e8cc9adf1394d787
Joseph Soria <josephsoria6@gmail.com>
2021-05-06 22:35:18 -0500

    set up App with browser router and page comps

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,23 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Main from '../src/layouts/Main';
+import Home from './pages/Home';
+import Search from './pages/Search';
+import Saved from './pages/Saved';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Main />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/search" element={<Search />} />
+            <Route path="/saved" element={<Saved />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
