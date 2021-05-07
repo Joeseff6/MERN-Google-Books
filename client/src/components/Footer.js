eb8aa0a942706fe36e375a15cabb9e52f300332f
Joseph Soria <josephsoria6@gmail.com>
2021-05-06 20:00:31 -0500

    made footer component

---
+++ client/src/components/Footer.js
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <div className="footer">
+      <span>
+        Powered by the powerful React Library!
+      </span>
+    </div>
+  )
+}
+
+export default Footer;
