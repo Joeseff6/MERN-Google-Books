9510ce025f3cdbdd3a5c46b40df67546294013ae
Joseph Soria <josephsoria6@gmail.com>
2021-05-06 22:43:58 -0500

    changed for attr to htmlFor

--- client/src/components/Forms/SignUpForm.js
+++ client/src/components/Forms/SignUpForm.js
@@ -5,11 +5,11 @@ function SignUpForm() {
       <h2 className="text-center">Sign Up</h2>
       <div className="form-floating mb-3">
         <input type="text" className="form-control" id="floatingInputSignUp" placeholder="user123" />
-        <label for="floatingInputSignUp">Username</label>
+        <label htmlFor="floatingInputSignUp">Username</label>
       </div>
       <div className="form-floating">
         <input type="password" className="form-control" id="floatingPasswordSignUp" placeholder="Password" />
-        <label for="floatingPasswordSignUp">Password</label>
+        <label htmlFor="floatingPasswordSignUp">Password</label>
       </div>
       <button 
       className="btn d-block m-auto mt-3" 
