81944b004546a68a70c1588e9a994837bdc4c588
Joseph Soria <josephsoria6@gmail.com>
2021-05-06 22:44:14 -0500

    added blank function handleClick

--- client/src/components/Forms/LoginForm.js
+++ client/src/components/Forms/LoginForm.js
@@ -1,19 +1,23 @@
 function LoginForm() {
 
+  function handleClick() {
+  }
+
   return (
     <>
       <h2 className="text-center">Log in</h2>
       <div className="form-floating mb-3">
         <input type="text" className="form-control" id="floatingInputLogin" placeholder="user123" />
-        <label for="floatingInputLogin">Username</label>
+        <label htmlFor="floatingInputLogin">Username</label>
       </div>
       <div className="form-floating">
         <input type="password" className="form-control" id="floatingPasswordLogin" placeholder="Password" />
-        <label for="floatingPasswordLogin">Password</label>
+        <label htmlFor="floatingPasswordLogin">Password</label>
       </div>
       <button 
       className="btn d-block m-auto mt-3" 
       type="submit"
+      onClick={handleClick}
       >
         Submit
       </button>
