bf81761e7911804e2e185b28cc717e7901798eb6
Joseph Soria <josephsoria6@gmail.com>
2021-05-08 21:31:35 -0500

    made function to connect to DB via API call

--- client/src/components/Forms/LoginForm.js
+++ client/src/components/Forms/LoginForm.js
@@ -1,28 +1,74 @@
-function LoginForm() {
+import { useState, useEffect } from "react";
+const API = require("../../utils/API");
 
-  function handleClick() {
+function LoginForm() {
+  let [body, setBody] = useState({});
+  let [username, setUsername] = useState();
+  let [password, setPassword] = useState();
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      document.getElementById("floatingInputLogin").value = "";
+      document.getElementById("floatingPasswordLogin").value = "";
+      await API.login(body);
+      window.location.pathname = "/search";
+    } catch (err) {
+      document.getElementById("errorLogin").classList.remove("d-none")
+      console.log(err);
+    }
   }
+
+  function handleChange({ target }) {
+    if (target.type === "text") {
+      setUsername(target.value);
+    }
+    if (target.type === "password") {
+      setPassword(target.value);
+    }
+  }
+
+  useEffect(() => {
+    setBody({ username: username, password: password });
+  }, [username, password]);
 
   return (
     <>
+      <div className="row text-center mb-3 d-none" id="errorLogin">
+        <span className=" error">Incorrect username or password!</span>
+      </div>
       <h2 className="text-center">Log in</h2>
-      <div className="form-floating mb-3">
-        <input type="text" className="form-control" id="floatingInputLogin" placeholder="user123" />
-        <label htmlFor="floatingInputLogin">Username</label>
-      </div>
-      <div className="form-floating">
-        <input type="password" className="form-control" id="floatingPasswordLogin" placeholder="Password" />
-        <label htmlFor="floatingPasswordLogin">Password</label>
-      </div>
-      <button 
-      className="btn d-block m-auto mt-3" 
-      type="submit"
-      onClick={handleClick}
-      >
-        Submit
-      </button>
+      <form>
+        <div className="form-floating mb-3">
+          <input
+            onChange={handleChange}
+            type="text"
+            className="form-control"
+            id="floatingInputLogin"
+            placeholder="user123"
+          />
+          <label htmlFor="floatingInputLogin">Username</label>
+        </div>
+        <div className="form-floating">
+          <input
+            onChange={handleChange}
+            type="password"
+            className="form-control"
+            id="floatingPasswordLogin"
+            placeholder="Password"
+          />
+          <label htmlFor="floatingPasswordLogin">Password</label>
+        </div>
+        <button
+          className="btn d-block m-auto mt-3"
+          type="submit"
+          onClick={handleSubmit}
+        >
+          Submit
+        </button>
+      </form>
     </>
-  )
+  );
 }
 
 export default LoginForm;
