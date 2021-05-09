21220231aebbb067e33ac8171dbfaf46cc0d5ff5
Joseph Soria <josephsoria6@gmail.com>
2021-05-09 11:37:21 -0500

    wrote function to send GET request for books

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -7,6 +7,16 @@ const API = {
 
   signUp: function(body) {
     return axios.post("/api/users", {data: body});
+  },
+
+  logout: function() {
+    return axios.post("/api/users/logout")
+  },
+
+  getBooks: function(query) {
+    const splitQuery = query.split(" ");
+    const joinQuery = splitQuery.join("%20");
+    return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${joinQuery}&maxResults=10&orderBy=relevance`);
   }
 };
 
