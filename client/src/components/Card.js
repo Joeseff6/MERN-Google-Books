651719c5d65193a457ea4e07fbc5dd8064a846c5
Joseph Soria <josephsoria6@gmail.com>
2021-05-09 11:39:22 -0500

    Started Card component

---
+++ client/src/components/Card.js
@@ -0,0 +1,13 @@
+function Card(props) {
+  return(
+    <>
+      <div className="row">
+        <div className="col-8 bookCard m-auto">
+          <span>hey</span>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Card;
