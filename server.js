30e8d440964239b2510cb66add3510cb7eb9e4f4
Joseph Soria <josephsoria6@gmail.com>
2021-05-09 11:40:25 -0500

    imported dotenv and ran config

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
+require('dotenv').config();
+
 
 const PORT = process.env.PORT || 3001;
 
