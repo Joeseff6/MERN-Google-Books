14413606fd91d53b16e08887b6c6d72228217995
Joseph Soria <josephsoria6@gmail.com>
2021-05-08 21:32:28 -0500

    correct req.body

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -4,8 +4,8 @@ const bcrypt = require(`bcrypt`);
 
 router.post(`/`, async (req, res) => {
   try {
-    req.body.password = await bcrypt.hash(req.body.password, 10);
-    const newUser = await db.User.create(req.body);
+    req.body.data.password = await bcrypt.hash(req.body.data.password, 10);
+    const newUser = await db.User.create(req.body.data);
     req.session.userId = newUser._id;
     req.session.loggedIn = true;
     res.status(200).json({ user: newUser, message: "New user created" });
@@ -17,14 +17,14 @@ router.post(`/`, async (req, res) => {
 
 router.post('/login', async (req, res) => {
   try {
-    const userData = await db.User.findOne({ username: req.body.username });
+    const userData = await db.User.findOne({ username: req.body.data.username });
     if (!userData) {
       res
         .status(400)
         .json({ message: 'Incorrect username or password, please try again' });
       return;
     }
-    bcrypt.compare(req.body.password, userData.password, (err, isMatching) => {
+    bcrypt.compare(req.body.data.password, userData.password, (err, isMatching) => {
       if (err) {
         console.log(err);
       } else if (isMatching) {
@@ -48,7 +48,7 @@ router.post('/login', async (req, res) => {
 router.post('/logout', (req, res) => {
   if (req.session.loggedIn) {
     req.session.destroy(() => {
-      res.status(204).end();
+      res.redirect('/');
     });
   } else {
     res.status(404).end();
