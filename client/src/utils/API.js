1c0b25512a25dde00b93348b22f45d8ff7d26dbb
Joseph Soria <josephsoria6@gmail.com>
2021-05-11 03:10:44 -0500

    added save book, get user book, and delete book

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 
 const API = {
   login: function(body) {
-    return axios.post("/api/users/login", {data: body})
+    return axios.post("/api/users/login", {data: body});
   },
 
   signUp: function(body) {
@@ -10,13 +10,25 @@ const API = {
   },
 
   logout: function() {
-    return axios.post("/api/users/logout")
+    return axios.post("/api/users/logout");
   },
 
   getBooks: function(query) {
     const splitQuery = query.split(" ");
     const joinQuery = splitQuery.join("%20");
     return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${joinQuery}&maxResults=10&orderBy=relevance&printType=books`);
+  },
+
+  saveBook: function(body) {
+    return axios.post("/api/books", {data: body});
+  },
+
+  getUserBooks: function() {
+    return axios.get("/api/books");
+  },
+
+  deleteBook: function(id) {
+    return axios.delete("api/books/" + id)
   }
 };
 
