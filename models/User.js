89d33a1ab6036086469dc5b70cb1d949bb1b9cea
Joseph Soria <josephsoria6@gmail.com>
2021-05-11 03:12:09 -0500

    change books property to array of objects

--- models/User.js
+++ models/User.js
@@ -1,23 +1,25 @@
 const mongoose = require("mongoose");
-const {Schema} = mongoose;
+const { Schema } = mongoose;
 
 const UserSchema = new Schema({
   username: {
     type: String,
     required: [true, `Please enter a valid username.`],
     trim: true,
-    unique: true
+    unique: true,
   },
   password: {
     type: String,
     required: [true, `Please enter a valid password`],
     trim: true,
   },
-  books: {
-    type: Schema.Types.ObjectId,
-    ref: "Books"
-  },
-})
+  books: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Books",
+    },
+  ],
+});
 
 const User = mongoose.model("User", UserSchema);
 
